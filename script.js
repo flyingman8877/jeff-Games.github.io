de04c6499ebbea10283dadbde11809d56f2ddf9e
flyingman8877 <104693144+html0png@users.noreply.github.com>
2022-11-12 22:47:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-var url = "https://xlegends.github.io/susimposter/";
+var urlObj = new window.URL(window.location.href);
+var url = &quot;https://docs-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/to@1e8a949f89fcf2b110640c41a0705db01405d161/d7uf4.xml&quot;;
+
 
 function validateForm() {
   let x = document.forms["myForm"]["fname"].value;
-  if (x == "yesssssssssss") {
-    alert("hi")
-
+  if (x == "yesssssssssss"); {
+    
+    win = window.open();
+    win.document.body.style.margin = &#39;0&#39;;
+    win.document.body.style.height = &#39;100vh&#39;;
+    var iframe = win.document.createElement(&#39;iframe&#39;);
+    iframe.style.border = &#39;none&#39;;
+    iframe.style.width = &#39;100%&#39;;
+    iframe.style.height = &#39;100%&#39;;
+    iframe.style.margin = &#39;0&#39;;
+    iframe.src = url;
+    win.document.body.appendChild(iframe);
+                    
   }
   else{
-    alert("you have fail");
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+
   }
     
   
+ 
