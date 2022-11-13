bad07e991099fac664348a74aadd3549a97bf154
flyingman8877 <104693144+html0png@users.noreply.github.com>
2022-11-12 20:59:39 -0600

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,5 @@
+please do not take any thing from this website with out shouting out flyingman8877 and https://codepen.io/Adir-SL thank you 
+
+thank you have a good day
+
+if you have any questions add them in the coments
---
+++ script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+  let x = document.forms["myForm"]["fname"].value;
+  if (x == "yesssssssssss") {
+    
+  }
+  else{
+    alert("you have fail");
+    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }
+    
+  
